35225466f3f25c092881160a53b544ff7f0c21e0
Chris Kampmeier <chris@kampers.net>
2009-02-06 23:42:29 -0800

    Rails!

---
+++ config/environment.rb
@@ -0,0 +1,18 @@
+# Be sure to restart your server when you modify this file
+
+# Specifies gem version of Rails to use when vendor/rails is not present
+RAILS_GEM_VERSION = '2.3.0' unless defined? RAILS_GEM_VERSION
+
+# Bootstrap the Rails environment, frameworks, and default configuration
+require File.join(File.dirname(__FILE__), 'boot')
+
+Rails::Initializer.run do |config|
+
+  config.gem "mysql",       :version => "2.7"
+  config.gem "rspec-rails", :version => "1.1.12", :lib => false
+  
+  
+  config.frameworks -= [ :active_resource, :action_mailer ]
+
+  config.time_zone = 'UTC'
+end
