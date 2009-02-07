e74d39a90333dd185e1547f34a3712b1df53ffaa
Chris Kampmeier <chris@kampers.net>
2009-02-07 14:25:36 -0800

    Add paperclip

--- config/environment.rb
+++ config/environment.rb
@@ -8,10 +8,11 @@ require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
 
-  config.gem "mysql",       :version => "2.7"
-  config.gem "rspec-rails", :version => "1.1.12", :lib => false
-  config.gem "capistrano",  :version => "2.5.4",  :lib => false
-  config.gem "rack",        :version => "0.9.1",  :lib => false
+  config.gem "mysql",                :version => "2.7"
+  config.gem "thoughtbot-paperclip", :version => "2.2.2",  :lib => "paperclip", :source => "http://gems.github.com"
+  config.gem "rspec-rails",          :version => "1.1.12", :lib => false
+  config.gem "capistrano",           :version => "2.5.4",  :lib => false
+  config.gem "rack",                 :version => "0.9.1",  :lib => false
 
   config.frameworks -= [ :active_resource ]
 
