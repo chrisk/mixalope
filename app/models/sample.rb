3ac8445595a1b5db0729c1cbff8bcddc767491ad
Chris Kampmeier <chris@kampers.net>
2009-02-09 22:36:41 -0800

    Add upload validations. Momentary TATFT lapse

--- app/models/sample.rb
+++ app/models/sample.rb
@@ -3,4 +3,7 @@ class Sample < ActiveRecord::Base
   has_attached_file :sound, :url =>  "/uploads/:class/:id_partition/:style.:extension",
                             :path => ":rails_root/public/uploads/:class/:id_partition/:style.:extension"
 
+  validates_attachment_presence :sound
+  validates_attachment_content_type :sound, :content_type => "audio/mpg"
+  validates_attachment_size :sound, :less_than => 1.megabyte
 end
