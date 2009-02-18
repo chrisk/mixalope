d9af455c69ff73ff5702b4fb65e00ceef41bfad2
Chris Kampmeier <chris@kampers.net>
2009-02-18 03:44:39 -0800

    Clean up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
 ActionController::Routing::Routes.draw do |map|
-  map.resources :sample_usages
-
-  map.resources :drum_machines
-
-  map.resources :samples
-
+  map.resources :drum_machines, :samples
 
   map.root :controller => "pages", :action => "frontpage"
-
 end
